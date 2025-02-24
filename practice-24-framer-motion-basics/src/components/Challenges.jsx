f24f1611e9255e1e9e4a7c1c111b9df895c1a40c
Sang Do <dovansang97@gmail.com>
2025-02-24 13:45:30 +0700

    add practice-24-framer-motion-basics

---
+++ practice-24-framer-motion-basics/src/components/Challenges.jsx
@@ -0,0 +1,69 @@
+import { useContext, useState } from "react";
+
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import ChallengeItem from "./ChallengeItem.jsx";
+import ChallengeTabs from "./ChallengeTabs.jsx";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function Challenges() {
+  const { challenges } = useContext(ChallengesContext);
+  const [selectedType, setSelectedType] = useState("active");
+  const [expanded, setExpanded] = useState(null);
+
+  function handleSelectType(newType) {
+    setSelectedType(newType);
+  }
+
+  function handleViewDetails(id) {
+    setExpanded((prevId) => {
+      if (prevId === id) {
+        return null;
+      }
+
+      return id;
+    });
+  }
+
+  const filteredChallenges = {
+    active: challenges.filter((challenge) => challenge.status === "active"),
+    completed: challenges.filter(
+      (challenge) => challenge.status === "completed"
+    ),
+    failed: challenges.filter((challenge) => challenge.status === "failed"),
+  };
+
+  const displayedChallenges = filteredChallenges[selectedType];
+
+  return (
+    <div id="challenges">
+      <ChallengeTabs
+        challenges={filteredChallenges}
+        onSelectType={handleSelectType}
+        selectedType={selectedType}
+      >
+        <AnimatePresence mode="wait">
+          {" "}
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+              key="list"
+              exit={{ y: -30, opacity: 0 }}
+              className="challenge-items"
+            >
+              {displayedChallenges.map((challenge) => (
+                <ChallengeItem
+                  key={challenge.id}
+                  challenge={challenge}
+                  onViewDetails={() => handleViewDetails(challenge.id)}
+                  isExpanded={expanded === challenge.id}
+                />
+              ))}
+            </motion.ol>
+          )}
+          {displayedChallenges.length === 0 && (
+            <p key="fallback">No challenges found.</p>
+          )}
+        </AnimatePresence>
+      </ChallengeTabs>
+    </div>
+  );
+}
